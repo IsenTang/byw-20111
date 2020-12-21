293713ee646987ec782524f1fa239dd94200d5d8
isenTang <3104255643@qq.com>
2020-12-21 12:16:57 +0800

    驼峰命名

--- 第二阶段/day01/index.js
+++ 第二阶段/day01/index.js
@@ -15,3 +15,15 @@ console.log('isen 说:"大家好"')
 
 // * 转义字符
 console.log('isen 说:\'大家好\'');
+
+
+// * 变量
+var pi = 3.1415926;
+pi * 10 * 10;
+
+pi * 20 * 20;
+
+// * 驼峰命名
+var getTeacherName;
+
+var getteachername;
