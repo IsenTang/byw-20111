e0d1397efdfc27d626027dc010efe9d65852c4ff
isenTang <3104255643@qq.com>
2020-12-21 10:54:01 +0800

    转义字符

---
+++ 第二阶段/day01/index.js
@@ -0,0 +1,17 @@
+// * 控制台打印
+console.log('hello world');
+
+// * 字面量
+// * 字符串
+'hello';
+
+// * 数字
+123;
+
+console.log(123);
+
+// * 单双引号嵌套
+console.log('isen 说:"大家好"')
+
+// * 转义字符
+console.log('isen 说:\'大家好\'');
