51a603ce1712a9bd8225aa0adeec6c78cbf8e24b
isenTang <3104255643@qq.com>
2020-12-21 14:37:59 +0800

    动画 & 基本数据类型

--- 第二阶段/day01/index.js
+++ 第二阶段/day01/index.js
@@ -27,3 +27,73 @@ pi * 20 * 20;
 var getTeacherName;
 
 var getteachername;
+
+// ===============
+// * 基本数据类型
+
+// * 字符串 string
+var str = `反引号`;
+
+var str1 = 'str1 字符串';
+console.log('str1: ', str1);
+var str2 = 'str2 字符串';
+console.log('str2: ', str2);
+
+// * 连接字符串
+console.log(str1 + str2);
+
+// * vscode 插件： better comments
+// ! warning
+// ? question
+// todo todo
+// * normal
+
+// * vscode 插件：javascript console utils
+var a = 'a';
+console.log('a: ', a);
+
+// * 数字 number
+var n1 = 123;
+var n2 = 123.123;
+
+// * 超过最大值
+var infinityNum = Infinity;
+
+// * 不是数字
+var notNumber = NaN;
+
+console.log("a" * "b");
+
+// * 精度丢失
+var result = 0.1 + 0.2;
+console.log('result ===>',result);
+
+result = (1 + 2)/10;
+console.log('result ===>',result);
+
+// * 布尔值
+var flag = true;
+console.log('flag: ', flag);
+flag = false;
+console.log('flag: ', flag);
+
+// * undefined
+var value;
+console.log('value: ', value);
+
+// * null
+var nullValue = null;
+console.log('nullValue: ', nullValue);
+
+// * typeof 类型检查
+console.log('typeof flag ===>',typeof flag);
+console.log('typeof n1 ===>',typeof n1);
+
+var aNum = 'a'
+var bNum = 'b'
+
+if(typeof aNum === 'number' 
+&& typeof bNum === 'number'){
+    
+    console.log(aNum * bNum);
+}
