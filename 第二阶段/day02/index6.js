1d41eb0fe316b0c223fe8b00bb1c936561bfbe83
isenTang <3104255643@qq.com>
2020-12-22 17:14:00 +0800

    运算符优先级

--- 第二阶段/day02/index6.js
+++ 第二阶段/day02/index6.js
@@ -1,3 +1,12 @@
 true ? console.log(111) : console.log(2);
 
 1 > 2 ? console.log('1大') : console.log('2大');
+
+
+var a = 300;
+var b = 200;
+var c = 100;
+var max = a > b ? (a > c ? a : c ) 
+                :  (b > c ? b : c );
+
+console.log(max);
---
+++ 第二阶段/day02/index7.js
@@ -0,0 +1,11 @@
+console.log( 3 > 2 > 1 - 0 );
+
+
+var dd = "test";
+console.log('dd is ' + (dd === "test") ? 1 : 2);
+
+console.log('dd is ' + (true) ? 1 : 2);
+
+console.log('dd is true' ? 1 : 2);
+
+console.log( true ? 1 : 2);
