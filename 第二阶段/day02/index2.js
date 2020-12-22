1480901c28b6554ee8d974297d50c24cc84bede2
isenTang <3104255643@qq.com>
2020-12-22 14:14:57 +0800

    非

---
+++ 第二阶段/day02/index2.js
@@ -0,0 +1,13 @@
+var flag = false;
+
+console.log(!flag);
+
+var value1 = 10;
+
+console.log(!value1);
+console.log(!Boolean(value1));
+
+var value2 = 10;
+
+console.log('value2 ===>',!!value2);
+console.log('value2 ===>',Boolean(value2));
