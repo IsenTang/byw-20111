d259fb0e97c9f399f8e2f619f5fb943a8766b28b
isenTang <3104255643@qq.com>
2020-12-23 15:08:03 +0800

    循环

---
+++ 第二阶段/day03/index5.js
@@ -0,0 +1,37 @@
+
+// * 循环语句
+// for(var i = 1; i <= 10; i++ ){
+//     // console.log(i);
+//     // console.log('加油');
+// }
+
+// * break
+// for(var i = 0; i < 10; i++ ){
+//     // if(i <= 5){
+//     //     console.log(i);
+//     // }
+//     console.log(i);
+//     if(i > 4){
+//         break;
+//     }
+//     console.log('执行次数===>',i);
+// }
+
+var array = 
+["王梓人",
+"孙善成",
+"方启豪",
+"李杨",
+"邓易晴",
+"狄春燕"];
+
+for(var i = 0; i < array.length; i++ ){
+
+    console.log(array[i]);
+    if(array[i] === '李杨'){
+
+        console.log(array[i]);
+        break;
+    };
+}
+
