cc8d897deff09a51a85c22f157033fd8c45287c8
isenTang <3104255643@qq.com>
2020-12-28 11:06:27 +0800

    stack

--- 第二阶段/day06/index4.js
+++ 第二阶段/day06/index4.js
@@ -1,17 +1,81 @@
+// function a(){
+
+//     function b(){
+
+//         console.log('b');
+//         function c(){
+
+//             console.log('cccc');
+//         }
+
+//         c();
+//     }
+
+//     b();
+// }
+
+// a();
+
+// function Stack(){
+
+//     this.data = [];
+
+//     this.push = function(ele){
+
+//         this.data.push(ele);
+//     }
+
+//     // * 弹出
+//     this.pop = function(){
+
+//         this.data.pop();
+//     }
+
+//     this.top = function(){
+
+//         return this.data[this.data.length - 1];
+//     }
+
+//     this.isEmpty = function(){
+
+//         return this.data.length === 0;
+//     }
+
+//     this.size = function(){
+
+//         return this.data.length
+//     }
+
+//     this.clear = function(){
+
+//         this.data = [];
+//     }
+// }
+
+// var s = new Stack();
+
+// s.push(1);
+// s.push(2);
+// // s.pop();
+// console.log(s.data);
+// console.log(s.top());
+
 function a(){
+
+    var aValue = 'aaaa';
 
     function b(){
 
-        console.log('b');
-        function c(){
-
-            console.log('cccc');
-        }
-
-        c();
+        console.log(aValue);
     }
 
-    b();
+    // var b = function(){
+    //     console.log(aValue);
+    // }
+
+    return b;
 }
 
-a();
+var bFunc = a();
+bFunc();
+
