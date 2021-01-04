b4ad33f8bbfb68ceff83290851fe27f1281c2a6a
isenTang <3104255643@qq.com>
2021-01-04 10:01:05 +0800

    jquery选择器

--- 第二阶段/day10/index.js
+++ 第二阶段/day10/index.js
@@ -1,1 +1,41 @@
-console.log($);
+// * 原生的方法
+var ele = document.getElementById('test');
+
+document.getElementsByClassName('test');
+
+// * 类选择器
+$('.test')
+
+// * id选择器
+$('#test');
+
+// * 元素选择器
+$('div')
+document.getElementsByTagName('div');
+
+// * 层级选择器
+$('.test > div');
+// console.log('===>', $('.test > div'));
+
+$(".test div");
+// console.log('===>', $('.test div'));
+
+// * 过滤
+// console.log($('div:empty'));
+
+// console.log($("div:contains('123')"));
+
+// * 属性操作
+var inputEle = document.getElementById('input');
+inputEle.getAttribute('value');
+
+// inputEle.value;
+$('#input').attr('value');
+console.log('value ==>',$('#input').attr('value'));
+
+inputEle.setAttribute('value','.....');
+$('#input').attr('value',function(){
+    return 'after-set';
+});
+
+$('#input').removeAttr('value');
