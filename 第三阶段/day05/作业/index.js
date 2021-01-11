ed02b15880a860268d135eb128f7446cf6065754
isenTang <3104255643@qq.com>
2021-01-11 15:13:53 +0800

    ajax

---
+++ 第三阶段/day05/作业/index.js
@@ -0,0 +1,21 @@
+const data1 = {
+    code : 200, // ===>
+    message : 'this is success.' // ==>
+}
+
+const data2 = {
+    code : 404, // ===>
+    message : 'failed' // ==>
+}
+
+if(data.code === 200){
+    // ...
+}
+
+if(data.message === 'this is success'){
+
+}
+
+if(data.code === 404 ){
+    // ...
+}
