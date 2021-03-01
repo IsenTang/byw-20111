f9d1137a97c86cee6f71e712ccd605de5553569a
JiangWen <13089848409@163.com>
2021-03-01 11:04:28 +0800

    day6-work

--- 第五阶段/周六作业/work/src/App.vue
+++ 第五阶段/周六作业/work/src/App.vue
@@ -32,7 +32,7 @@
                         
 
                         <!-- 任务 -->
-                        <div v-for="(item, index) in showList" :key="index" class="todo">
+                        <!-- <div v-for="(item, index) in showList" :key="index" class="todo">
                             <div @click="changeState(item)" class="chos-div">
                                 <div v-if="item.isCom">✓</div>
                             </div>
@@ -41,18 +41,11 @@
                             <div>
                                 <button @click="deleteThis(item)" class="destroy"></button>
                             </div>
-                        </div>
-
-
-                        <!-- <div class="todo">
-                            <div class="chos-div">
-                                <div>✓</div>
-                            </div>
-                            <p style="color:#666666;">未完成任务</p>
-                            <div>
-                                <button class="destroy"></button>
-                            </div>
                         </div> -->
+
+                        <template v-for="(item, index) in showList" >
+                            <item-todo :item="item" :key="index" @deleteFun="deleteThis"></item-todo>
+                        </template>
 
                     </div>
                 </div>
@@ -64,6 +57,7 @@
 </template>
 
 <script>
+import ItemTodo from "./components/itemTodo"
 
 var store = {
     get (name) {
@@ -77,7 +71,7 @@ var store = {
 export default {
     name: 'App',
     components: {
-
+        'item-todo': ItemTodo
     },
     data () {
         return {
@@ -145,13 +139,12 @@ export default {
             }
             return arr.length
         },
-        changeState (item) {
-            item.isCom = !item.isCom;
-        },
-        deleteThis(item) {
+        // changeState (item) {
+        //     item.isCom = !item.isCom;
+        // },
+        deleteThis (item) {
             console.log('item', item)
             let index = this.list.indexOf(item);
-            console.log('index', index)
             this.list.splice(index, 1);
         }
     },
@@ -296,81 +289,4 @@ body {
 
     }
 
-
-    .todo {
-        background-color: #ffffff;
-        position: relative;
-        font-size: 16px;
-        border-bottom: 1px solid #ededed;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 10px;
-
-        &:hover {
-            background-color: #fafafa;
-            & .destroy {
-                display: block;
-            }
-        }
-
-        .chos-div {
-            width: 16px;
-            height: 16px;
-            border: solid 1px #83bfe8;
-            border-radius: 4px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            div {
-                font-size: 10px;
-                color: #296994;
-
-            }
-        }
-
-        p {
-            font-size: 14px;
-            font-weight: 500;
-            max-width: 80%;
-        }
-
-        button {
-            margin: 0;
-            padding: 0;
-            border: 0;
-            background: 0;
-            font-size: 100%;
-            vertical-align: baseline;
-            font-family: inherit;
-            font-weight: inherit;
-            color: inherit;
-            outline: 0
-        }
-
-        .destroy {
-            display: none;
-            position: absolute;
-            top: 50%;
-            transform: translate(0, -50%);
-            right: 10px;
-            width: 40px;
-            height: 40px;
-            margin: auto 0;
-            font-size: 30px;
-            color: #cc9a9a;
-            margin-bottom: 11px;
-            transition: color .2s ease-out;
-
-            &:hover {
-                color: #af5b5e;
-            }
-
-            &:after {
-                content: '×';
-            }
-        }
-    }
 </style>
